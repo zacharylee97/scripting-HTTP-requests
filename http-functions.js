2a43578ae7a6fbce6dfd73345049722e3f172c0a
Zachary Lee <zacharylhlee@gmail.com>
2018-10-01 16:03:45 +0000

    Fixed problem with printing data

--- http-functions.js
+++ http-functions.js
@@ -2,8 +2,8 @@ module.exports = function getHTML (options, callback) {
   var https = require('https');
   https.get(options, function(response) {
     response.setEncoding('utf8');
-    var output = "";
     response.on('data', function(data) {
+      var output = "";
       output += data;
       callback(output);
     });
